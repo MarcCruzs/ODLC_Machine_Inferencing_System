dcd8df52573f4e6e4e448a51f92be936cd8270dd
Joshua <student.joshuaestrada@gmail.com>
2023-11-07 16:50:01 -0800

    touched up rotation and scaling
    scaling and rotation are guaranteed to screw up the image due to aliasing and blurring and smudging and so on. applied different rotation and scaling methods to reduce this effect. also changed scaling to get the minimum scaling factor that still allows the image to be seen

--- SUAS_Competition_2024/object_recognition_src/ObjectCreation/scripts/image_combiner/combiner.py
+++ SUAS_Competition_2024/object_recognition_src/ObjectCreation/scripts/image_combiner/combiner.py
@@ -4,16 +4,55 @@ import os, random, datetime
 BG_PATH = 'E:\\2023\\synthetic_data_collection\\background'
 FG_PATH = 'E:\\2023\\synthetic_data_collection\\targetsWithAlphaNum'
 
+def get_scaling(name):
+    upper = 0.05
+    lower = 0.45
+
+    name = name.split('_')[1]
+
+    match name:
+        case "star":
+            lower = 0.045
+        case "cross":
+            lower = 0.0275
+        case "pentagon":
+            lower = 0.035
+        case "triangle":
+            lower = 0.035
+        case "rectangle":
+            lower = 0.0275
+        case "quartercircle":
+            lower = 0.0275
+        case "semicircle":
+            lower = 0.02125
+        case "circle":
+            lower = 0.0225
+    return random.uniform(lower, upper)
+
+def add_bounding_box(im, color, margin=5):
+    new_im = Image.new('RGBA', (fg_width + 2*margin, fg_height + 2*margin), (0, 0, 0, 0)) 
+    new_im.paste(im, (margin, margin))
+    w, h = new_im.size
+
+    draw = ImageDraw.Draw(new_im)
+    draw.line((0, 0, 0, h), fill=color, width=3)
+    draw.line((w, 0, w, h), fill=color, width=3)
+    draw.line((0, 0, w, 0), fill=color, width=3)
+    draw.line((0, h, w, h), fill=color, width=3)
+    return new_im
+
 # run in debugger or cmd or something
-while True:
+for i in range(1):
     # gets random image from random subfolder of base path
     rand_subfolder = os.path.join(BG_PATH, random.choice(os.listdir(BG_PATH)))
     rand_bg = os.path.join(rand_subfolder, random.choice(os.listdir(rand_subfolder)))
     rand_fg = os.path.join(FG_PATH, random.choice(os.listdir(FG_PATH)))
 
+    rand_bg = "E:\\2023\\synthetic_data_collection\\background\\a000\\Video_Breakdown43281.png"
+    rand_fg = "E:\\2023\\synthetic_data_collection\\targetsWithAlphaNum\\blue_circle_X_orange.png"
     # skips images whose text is same color as shape color
-    image_name = os.path.basename(rand_fg)
-    image_name = image_name.split('.')[0]
+    img_name = os.path.basename(rand_fg)
+    image_name = img_name.split('.')[0]
     image_name = image_name.split('_')
     if image_name[0] == image_name[-1]:
         # print(image_name[0], image_name[-1])
@@ -24,41 +63,31 @@ while True:
     foreground = Image.open(rand_fg)
 
     # scales down shapes
-    scale_factor = random.uniform(0.015, 0.03) # Range of 1.5% to 3% scaling
-    foreground = foreground.resize((int(foreground.size[0] * scale_factor), int(foreground.size[1] * scale_factor)))
+    # scale_factor = random.uniform(0.025, 0.04)
+    scale_factor = get_scaling(os.path.basename(img_name))
+    foreground = foreground.resize((int(foreground.size[0] * scale_factor), int(foreground.size[1] * scale_factor)), resample=Image.BILINEAR)
 
     # gets image dimensions
     bg_width, bg_height = background.size
     fg_width, fg_height = foreground.size
 
     # sets boundaries to prevent image from going outside the view
-    max_x = bg_width - (fg_width + 3)
-    max_y = bg_height - (fg_height + 3)
+    max_x = bg_width - (fg_width + 10)
+    max_y = bg_height - (fg_height + 10)
 
     # gets a random coordinate value that is less than the max
     coords = (random.randint(0, max_x), random.randint(0, max_y))
 
-
     now = datetime.datetime.now()
     formatted_now = now.strftime("%f")
 
-
-    # bounding box code
-
-    # creates outline
-    # rect_shape =  (0, 0, fg_width + 10, fg_height + 10)
-    # img = Image.new("RGBA", (fg_width + 10, fg_height + 10), (0,0,0,0))
-    # rect = ImageDraw.Draw(img)
-    # rect.rectangle(rect_shape, outline="yellow", width=3)
-    # rect_coords = (coords[0] - 5, coords[1] - 5)
-
-    # foreground.paste(img, (0,0), img)
+    foreground = add_bounding_box(foreground, 'Yellow')
 
     # creates 12 images, each rotated 30 degrees more than the previous
     for i in range(12):
         rotation = i * 30
         new_bg = background.copy()
-        new_fg = foreground.rotate(rotation, center=(foreground.size[0] / 2, foreground.size[1] / 2), expand=True)
+        new_fg = foreground.rotate(rotation, expand=True, resample=Image.BICUBIC)
         new_bg.paste(new_fg, coords, new_fg)
 
         # generates unique image name
