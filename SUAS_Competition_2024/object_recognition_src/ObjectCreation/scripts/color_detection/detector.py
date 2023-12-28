8a88106e8b4417087fa6360c6a266d394ec416c1
joestrada1022 <student.joshuaestrada@gmail.com>
2023-12-28 15:04:15 -0800

    created a functional version of rgb -> color name

--- SUAS_Competition_2024/object_recognition_src/ObjectCreation/scripts/color_detection/detector.py
+++ SUAS_Competition_2024/object_recognition_src/ObjectCreation/scripts/color_detection/detector.py
@@ -2,22 +2,26 @@ import cv2
 
 # use pre-defined rgb values for SUAS-specified colors
 COLORS = {
-    "red": (255, 0, 0), 
-    "green": (0, 255, 0), 
-    "blue": (0, 0, 255), 
-    "black": (0, 0, 0), 
+    "red": (255, 0, 0),
+    "green": (0, 255, 0),
+    "blue": (0, 0, 255),
+    "black": (0, 0, 0),
     "purple": (128, 0, 128),
     "white": (255, 255, 255),
     "orange": (255, 165, 0),
-    "brown": (150, 75, 0)
-    }
+    "brown": (150, 75, 0),
+}
+
 
 def vid2rgb(event, x, y, flags, param):
     if event == cv2.EVENT_MOUSEMOVE:
         colorsBGR = image[y, x]
         colorsRGB = tuple(reversed(colorsBGR))
         color_name = closest_color(colorsRGB, COLORS)
-        print(f"At, ({x},{y}), the closest color to RGB value {colorsRGB} is **{color_name}**.")
+        print(
+            f"At, ({x},{y}), the closest color to RGB value {colorsRGB} is **{color_name}**."
+        )
+
 
 def closest_color(requested_color, color_dict):
     min_colors = {}
@@ -29,12 +33,14 @@ def closest_color(requested_color, color_dict):
     return min_colors[min(min_colors.keys())]
 
 
-image = cv2.imread(r"SUAS_Competition_2024\object_recognition_src\ObjectCreation\testImages\black_pentagon_0_green.png")
-cv2.namedWindow('ColorDetection')
-cv2.setMouseCallback('ColorDetection', vid2rgb)
+image = cv2.imread(
+    r"SUAS_Competition_2024\object_recognition_src\ObjectCreation\testImages\black_pentagon_0_green.png"
+)
+cv2.namedWindow("ColorDetection")
+cv2.setMouseCallback("ColorDetection", vid2rgb)
 
-while(1):
-    cv2.imshow('ColorDetection', image)
+while 1:
+    cv2.imshow("ColorDetection", image)
     if cv2.waitKey(0):
         break
 
