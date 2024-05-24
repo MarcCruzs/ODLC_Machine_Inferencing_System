a1f188f302d874f715de8b26206121ec6f5cae87
MarC <MarcACruz916@gmail.com>
2024-05-23 18:34:13 -0700

    Saving Current Work

---
+++ ODLC_src/shape_human/src/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#get an image from volume 1
+image_file=$(find /raw_images -maxdepth 1 -type f -name "*.jpg" -print -quit)
+
+if [ -z "$image_file" ]; then
+    echo "no image found in volume"
+    exit 1
+fi
+
+#run HUMAN detection script
+python ./src/detect.py --source /raw_images/$(basename "$image_file") --weights ./src/HumanModel.pt --save-txt --name ModelResults
+
+#run Shape detection script
+python ./src/detect.py --source /raw_images/$(basename "$image_file") --weights ./src/ShapeModel.pt --save-txt --name ModelResults
+
+#copy image to volume 2 and remove it from first volume
+cp "$image_file" /imagebank
+
+rm /raw_images/$(basename "$image_file")
