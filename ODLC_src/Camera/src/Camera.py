fd058a54e1c1ea3541bb0cf427c4f048560a28af
Marc Cruz <MarcACruz916@gmail.com>
2024-07-30 22:09:11 -0700

    refactored Camera Class. For future reference, camera usage was designed for cmd line usage.

--- ODLC_src/Camera/src/Camera.py
+++ ODLC_src/Camera/src/Camera.py
@@ -1,20 +1,32 @@
 import argparse
+import logging
 import os
 import uuid
 
 import cv2
+import numpy as np
 
 
 class Camera:
+    logging.basicConfig(
+        filename="camera.log",
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - $(funcName)s -  %(message)s",
+    )
+
     def __init__(self, camera_device):
+        self.logger = logging.getLogger(__name__)
+
+        self.logger.warning("Camera Implementation may need its own docker test suite.")
+
         self.camera_device = camera_device
         self.cap = cv2.VideoCapture(self.camera_device)
-        print(f"Trying to open camera device {self.camera_device}...")
+        self.logger.info(f"Trying to open camera device {self.camera_device}")
 
         if not self.cap.isOpened():
             raise ValueError(f"Unable to open camera device {self.camera_device}")
         else:
-            print(f"Successfully opened camera device {self.camera_device}")
+            self.logger.info(f"Successfully opened camera device {self.camera_device}")
 
     def take_picture(self):
         print("Attempting to capture image...")
@@ -23,29 +35,31 @@ class Camera:
         if not ret:
             raise ValueError("Failed to capture image from camera")
 
-        print("Image captured successfully.")
+        self.logger.info("Image captured successfully.")
         return frame
 
-    def save_to_folders(self, image, folder1, folder2):
+    def save_to_folders(self, image: np.ndarray, raw_folder: str, archive_folder: str) -> None:
         filename = str(uuid.uuid4()) + ".jpg"
 
-        if not os.path.exists(folder1):
-            os.makedirs(folder1)
-        if not os.path.exists(folder2):
-            os.makedirs(folder2)
+        if not os.path.exists(raw_folder):
+            os.makedirs(raw_folder)
+        if not os.path.exists(archive_folder):
+            os.makedirs(archive_folder)
 
-        file_path1 = os.path.join(folder1, filename)
-        file_path2 = os.path.join(folder2, filename)
+        file_path1 = os.path.join(raw_folder, filename)
+        file_path2 = os.path.join(archive_folder, filename)
 
-        print(f"Saving image to {file_path1} and {file_path2}...")
+        self.logger.info(f"Saving image to {file_path1} and {file_path2}")
+
         cv2.imwrite(file_path1, image)
         cv2.imwrite(file_path2, image)
-        print("Image saved successfully.")
+
+        self.logger.info("Image saved successfully.")
 
     def release(self):
-        print("Releasing camera...")
+        self.logger.info("Releasing camera...")
         self.cap.release()
-        print("Camera released.")
+        self.logger.info("Camera released.")
 
 
 if __name__ == "__main__":
@@ -56,14 +70,14 @@ if __name__ == "__main__":
         default=int(os.getenv("CAMERA_DEVICE", 0)),
         help="Camera device index (e.g., 0, 1, 2) for windows. Camera pathway (i.g. /dev/video0) for linux.",
     )
-    parser.add_argument("--folder1", default="./Folder1", help="Path to folder1")
-    parser.add_argument("--folder2", default="./Folder2", help="Path to folder2")
+    parser.add_argument("--raw_folder", default="./raw_images", help="Path to raw_images")
+    parser.add_argument("--archive_folder", default="./archived_images", help="Path to archived_images")
     args = parser.parse_args()
 
+    camera = Camera(args.camera_device)
     try:
-        camera = Camera(args.camera_device)
         image = camera.take_picture()
-        camera.save_to_folders(image, folder1=args.folder1, folder2=args.folder2)
+        camera.save_to_folders(image, raw_folder=args.raw_folder, archive_folder=args.archive_folder)
     except ValueError as e:
         print(f"Error: {e}")
     finally:
